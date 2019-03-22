1e6cfa9a4b81d8b6bf23eb46d108a7cb5016c4d8
visualstudio <visualstudio@122-PC26>
2019-03-22 18:20:29 -0400

    Challenge 1

--- Hackathon/Program.cs
+++ Hackathon/Program.cs
@@ -10,13 +10,39 @@ namespace Hackathon
     {
         static void Main(string[] args)
         {
-            // Harpreet Kaur C0726031
-            // Mandeep Singh C0732152
-            // Gurjinder Singh C0730319
-            // Arshdeep singh C0716342
-            // Manvir singh C0730069
-            // Gurpinderpal singh C0731868
+            {
+                int t = 0;
+                string[] Names = new string[6] {"Harpreet Kaur","Mandeep Singh","Gurjinder Singh","Manvir Singh", "Arshdeep Singh", "Gurpinderpal Singh"};
+                string[] Letters = new string[6] { "A", "B", "C", "D", "E", "F" };
+                for (int b = 0; b <= 6; t++)
+                {
+                    Names[b] = Names[b] + " " + Letters[t];
+                }
+                t++;
+                for (int c = 4; c <= 7; c++)
+                {
+                    Names[c] = Names[c] + " " + Letters[t];
+                }
+                t++;
+                for (int l = 8; l <= 11; l++)
+                {
+                    Names[l] = Names[l] + " " + Letters[];
+                }
+                t++;
+                for (int m = 12; m <= 15; m++)
+                {
+                    Names[m] = Names[m] + " " + Letters[t];
+                }
+
+                for (int i = 0; i < Names.Length; i++)
+                {
+                    Console.WriteLine(Names[i]);
+                }
+
+
+                Console.ReadLine();
+            }
         }
     }
-
 }
+
